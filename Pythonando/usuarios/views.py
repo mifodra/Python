2a390163776edca196e3cb6f5750118fcca10dfa
mifodra <mifodra@gmail.com>
2024-01-21 14:44:47 -0300

    Salvando usuarios na tabela auth_user

--- Pythonando/usuarios/views.py
+++ Pythonando/usuarios/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if not senha == confirmar_senha:
+            return redirect('/usuarios/cadastro')
+        
+        user = User.objects.filter(username=username)
+        if user.exists():
+            return redirect('/usuarios/cadastro')
+        
+        try:    
+            User.objects.create_user(
+                username= username,
+                password= senha
+            )
+            return redirect('/usuarios/login') #Vai dar erro ainda
+        except: 
+            return redirect('/usuarios/cadastro')
