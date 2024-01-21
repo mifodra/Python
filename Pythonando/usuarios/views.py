e6d56a3af455ea35afd8148a268d973af5d028cd
mifodra <mifodra@gmail.com>
2024-01-21 15:22:33 -0300

    Criando o login de usuarios da aplicação

--- Pythonando/usuarios/views.py
+++ Pythonando/usuarios/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.messages import constants
 from django.contrib import messages
+from django.contrib import auth
 
 # Create your views here.
 
@@ -28,7 +29,24 @@ def cadastro(request):
                 username= username,
                 password= senha
             )
-            return redirect('/usuarios/login') #Vai dar erro ainda
+            return redirect('/usuarios/logar')
         except: 
             messages.add_message(request, constants.ERROR, 'Erro interno do servidor')
             return redirect('/usuarios/cadastro')
+        
+def logar(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = auth.authenticate(request, username=username, password=senha)
+
+        if user:
+            auth.login(request, user)
+            messages.add_message(request, constants.SUCCESS, 'Logado!')
+            return redirect('/flashcard/novo_flashcard/') #Vai dar erro ainda
+        else:
+            messages.add_message(request, constants.ERROR, 'Username ou senha inválidos')
+            return redirect('/usuarios/logar')
