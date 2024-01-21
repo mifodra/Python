c7c0deff9cbcbb492f48751db49e75b7afe102d1
mifodra <mifodra@gmail.com>
2024-01-21 14:55:18 -0300

    Finalizando parte de Cadastros da aplicação

--- Pythonando/usuarios/views.py
+++ Pythonando/usuarios/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
 
 # Create your views here.
 
@@ -13,10 +15,12 @@ def cadastro(request):
         confirmar_senha = request.POST.get('confirmar_senha')
 
         if not senha == confirmar_senha:
+            messages.add_message(request, constants.ERROR, 'Senha e confirmar senha não coincidem')
             return redirect('/usuarios/cadastro')
         
         user = User.objects.filter(username=username)
         if user.exists():
+            messages.add_message(request, constants.ERROR, 'Usuário já existe')
             return redirect('/usuarios/cadastro')
         
         try:    
@@ -26,4 +30,5 @@ def cadastro(request):
             )
             return redirect('/usuarios/login') #Vai dar erro ainda
         except: 
+            messages.add_message(request, constants.ERROR, 'Erro interno do servidor')
             return redirect('/usuarios/cadastro')
